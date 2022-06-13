9bbdefb6dd8d189b75acad2dabea10417f49ca60
Akshat Giri <akshatgiri@gmail.com>
2022-06-12 18:58:01 -0700

    Updaitng video creator to use ffmpeg instead of cv2, 3x less video size and 2-3x faster video creation

--- VideoCreator/batch_videos.sh
+++ VideoCreator/batch_videos.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # change month to the month you want
-MONTH=10
+MONTH=5
 # change numbers to the dates that yoou want to cover
 for i in {1..31}
 do
   echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
   echo "-------------------------------------------------------------------------"
-  python3 video_creator.py /Users/akshatgiri/Documents/ScreenShooter/$MONTH-$i-2020 0
-  echo "-------------------------------------------------------------------------"
-  python3 video_creator.py /Users/akshatgiri/Documents/ScreenShooter/$MONTH-$i-2020 1
-  echo "-------------------------------------------------------------------------"
-  python3 video_creator.py /Users/akshatgiri/Documents/ScreenShooter/$MONTH-$i-2020 2
+  python3 video_creator.py /Users/akshatgiri/Documents/ScreenShooter/$MONTH-$i-2022 0
+  python3 video_creator.py /Users/akshatgiri/Documents/ScreenShooter/$MONTH-$i-2022 1
+  # echo "-------------------------------------------------------------------------"
+  # python3 video_creator.py /Users/akshatgiri/Documents/ScreenShooter/$MONTH-$i-2020 2
 done
--- VideoCreator/video_creator.py
+++ VideoCreator/video_creator.py
@@ -1,70 +1,112 @@
 # The purpose of this script is to take a series of 
 # pictures and convert it into video
 #
-# This is how it works -  
+# Prerequisites:
+# - python3
+# - ffmpeg installed on machine
+#
+# How the script works -  
 # This script takes in a folder path a command line argument
 # looks for all 0.jpg pictures recursively in that folder
 # sorts the images based on the time created
-# adds the images to the video creator 1 by 1 to keep the memory
-# usage low. And then we get a .mp4 file once the script is finished
+# create a txt file with paths to all sorted images ( frames for ffmpeg )
+# passes the txt file to ffmpeg to create a video
 # 
 # sample command = 
-# python3 video_creator.py /Users/akshatgiri/Documents/ScreenShooter/4-15-2020 
+# python3 video_creator.py /Users/akshatgiri/Documents/ScreenShooter/4-15-2020 0 
 
-import cv2
-import numpy as np
 import glob
 import os
 import sys
 from pathlib import Path
+import cProfile, pstats, io
+
+# Uncomment to profile the function
+# @profile
+def main():    
+    # only works for macos for now
+    def get_documents_path():
+        return f"{str(Path.home())}/Documents"
+
+    def get_output_folder_path():
+        return get_documents_path() + "/ScreenShooter/Finals"
+
+    if len(sys.argv) < 3:
+        raise Exception('Screenshots folder path and screen number are required.')
+
+    folder_path = sys.argv[1]
+    folder_name = os.path.basename(folder_path)
+    print(f"Searching in folder {folder_name}...")
+
+    screen_number = int(sys.argv[2])
+
+    # loading images from this folder
+    files = glob.glob(folder_path + f'/**/{screen_number}.jpg', recursive=True)
+    files.sort(key=lambda x: os.path.getmtime(x))
+
+    # making sure output folder exists
+    Path(get_output_folder_path()).mkdir(exist_ok=True)
+
+    # write a file with all image paths
+    print("Write a file with frames for ffmpeg")
+    video_frames_file = f'{folder_name}_{screen_number}.txt'
+
+    try:
+        # write file with all images as frames
+        with open(video_frames_file, 'w') as f:
+            for file in files:
+                # duration 0.125 is 8 frames per second.
+                f.write(f'file \'{file}\'\nduration 0.125\n')
+            # According to docs the last frame needs to be written twice, second time without a duration
+            f.write(f'file \'{file}\'')
+
+        # Using written file with frames to make a video with ffmpeg
+        output_file = get_output_folder_path() + "/" + folder_name + f' screen {screen_number + 1}.mp4'
+        os.system(f'ffmpeg -y -f concat -safe 0 -i \'{video_frames_file}\' -vsync vfr -pix_fmt yuv420p "{output_file}"')
+    except Exception as e:
+        print(e)
+    finally:
+        # delete the text file
+        os.remove(video_frames_file)
+        print("Done")
 
 
-# only works for macos for now
-def get_documents_path():
-    return f"{str(Path.home())}/Documents"
-
-def get_output_folder_path():
-    return get_documents_path() + "/ScreenShooter/Finals"
-
-if len(sys.argv) < 3:
-    raise Exception('Screenshots folder path and screen number are required.')
-
-folder_path = sys.argv[1]
-folder_name = os.path.basename(folder_path)
-print(f"Searching in folder {folder_name}...")
-
-screen_number = int(sys.argv[2])
-
-# loading images from this folder
-# TODO: Add support for creating video of second screen from terminal
-files = glob.glob(folder_path + f'/**/{screen_number}.jpg', recursive=True)
-files.sort(key=lambda x: os.path.getmtime(x))
-
-# just to set initial values for the video 
-img = cv2.imread(files[0])
-height, width, layers = img.shape
-size = (width,height)
-print(width, height)
-
-# making sure output folder exists
-Path(get_output_folder_path()).mkdir(exist_ok=True)
-
-# video output
-out = cv2.VideoWriter(get_output_folder_path() + "/" + folder_name + f' screen {screen_number + 1}.mp4', cv2.VideoWriter_fourcc(*'MP4V'), 8, size)
-
-current_img_processing = 1 # keeps track of how many images have been processed
-total_imgs = len(files)
-
-# loads the image using file path, 
-# ads the image to the vide0
-# moves on to the next image
-for filename in files:
-    print(f"Processin - {current_img_processing} of {total_imgs}")
-    current_img_processing += 1
-    img = cv2.imread(filename)
-    out.write(img)
+# Run main function 
+if __name__ == "__main__":
+    main()
 
 
-out.release()
+def profile(fnc):
+    
+    """A decorator that uses cProfile to profile a function"""
+    
+    def inner(*args, **kwargs):
+        
+        pr = cProfile.Profile()
+        pr.enable()
+        retval = fnc(*args, **kwargs)
+        pr.disable()
+        s = io.StringIO()
+        sortby = 'cumulative'
+        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        print(s.getvalue())
+        return retval
 
-print("Done")
+    return inner
+
+
+# SCRATCH PAD
+
+# This ffmpeg command will create a video from a series of images
+# The series of images will be in the ffmpeg_input.txt file with each image path and duration for each image.
+
+##
+# Example ffmpeg_input.txt file
+"""
+file 'test/04.43.38 PM/0.jpg'
+duration 0.125
+file 'test/04.43.38 PM/0.jpg'
+"""
+##
+# ffmpeg -y -f concat -safe 0 -i ffmpeg_input.txt -vsync vfr -pix_fmt yuv420p output.mp4
